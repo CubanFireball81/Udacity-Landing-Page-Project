ea01d671d4ea02975b96474f0d7f2be8cddf3e4b
Alejandro Santiago <cubanfireball81@gmail.com>
2021-12-05 13:10:17 -0900

    Updates to project including mobile responsiveness and navigation responsiveness

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+
+
 // Ensure page has been loaded before running functions.
 document.addEventListener('DOMContentLoaded', () => {
     // collect sections.
@@ -6,6 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     navigation(sections);
     // add menu items click event listener
     addMenuEventListener();
+    activeSection();
 })
 
 // build the nav menu from sections in html using for..of loop.
@@ -14,7 +17,7 @@ const navigation = (sections => {
         const sectionTitle = section.dataset.nav;
         createNavigation(sectionTitle);
     }
-})
+});
 
 // add eventlistener to nav links, then smoothly scroll to that section.
 const addMenuEventListener = () => {
@@ -22,11 +25,13 @@ const addMenuEventListener = () => {
     for (const menuList of menuLists) {
 
     // smoothly navigate to proper section when nav link is clicked.
-    menuList.addEventListener("click", () => {
-        const section = document.querySelector(`[data-nav='${menuList.id}']`);
-        if (section) {
+    menuList.addEventListener("click", (e) => {
+      // prevent eventListener's default action
+      e.preventDefault();
+      const section = document.querySelector(`[data-nav='${menuList.id}']`);
+      if (section) {
         section.scrollIntoView({ behavior: "smooth" });
-        }
+      }
     });
     }
 };
@@ -52,3 +57,93 @@ const createNavigation = (sectionName) => {
     let currentDiv = document.getElementById("navbar__list");
     currentDiv.appendChild(navBar);
 };
+
+// set a timer to track time
+let timer = null;
+
+//get the nav element's class so it can be targeted for hiding or showing the navbar
+const header = document.getElementsByClassName('page__header')[0];
+
+// function to check if the user is scrolling the page of not, if not then hide nav bar after 1.5 seconds. Also, clear the timer after scroll events so timer value doesn't keep increasing.
+function isScrolling() {
+  if(timer !== null) {
+    clearTimeout(timer);
+  }
+  timer = setTimeout(() => {
+    header.style.display = 'none';
+  }, 1500);
+}
+
+// Add class 'active' to section when it is near top of viewport
+const activeSection = () => {
+  const sections = document.querySelectorAll("section");
+  let linkName;
+  window.addEventListener("scroll", () => {
+    for (const section of sections) {
+      const box = section.getBoundingClientRect();
+      if (box.top < window.innerHeight/2 && box.bottom >= 0) {
+        removeActiveClass();
+
+        // Get's name to reference add/remove class active to/from
+        linkName = section.dataset.nav;
+        addActiveClass(section, linkName);
+      }
+    }
+  });
+};
+
+// Add active class to section and li elements
+const addActiveClass = (section, linkName) => {
+  section.classList.add(`section-active-class`);
+
+  // apply active class to link
+  const link = document.getElementById(linkName);
+  link.classList.add("link-active-class");
+};
+
+// Remove active class from section and li elements
+const removeActiveClass = () => {
+  const sections = document.querySelectorAll("section");
+  const links = document.querySelectorAll("li");
+  for (const section of sections) {
+    section.classList.remove(`section-active-class`);
+  }
+  for (const link of links) {
+    link.classList.remove(`link-active-class`);
+  }
+};
+
+// Hides navbar if user is not scrolling using isScrolling function. Defaults to false so navbar is showing at page load and until first scroll event.
+document.addEventListener('scroll', () => {
+  isScrolling();
+}, false);
+
+// Displays navbar when user scrolls the page
+document.addEventListener('scroll', () => {
+  header.style.display = 'block';
+});
+
+// Bring in the hamburger and navbar items so JS can listen to and toggle properties
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('#navbar__list');
+
+// Add hamburger click event listener
+hamburger.addEventListener('click', function() {
+  if(hamburger.classList == 'active') {
+    closeMenu();
+  } else {
+    openMenu();
+  }
+});
+
+// Toggle active classes to hamburger and openMenu
+function openMenu() {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+}
+
+// Remove active classes when hamburger close is clicked
+function closeMenu() {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+}
